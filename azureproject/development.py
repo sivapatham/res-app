d44e6fa14304628a604d9ddf368165f79bb39aa8
Sivapatham Muthaiah <sivapatham@in.ibm.com>
2022-11-02 17:47:00 +0530

    Updates

--- azureproject/development.py
+++ azureproject/development.py
@@ -9,38 +9,38 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = True
 
-tok_url = "https://myvault.secretsvaultcloud.com/v1/token"
-cred = {'grant_type':'password', 'username':'', 'password':'', 'provider':'thy-one'}
-response = requests.post(tok_url, data=cred)
-res = response.json()
-tok = res['accessToken']
+# tok_url = "https://myvault.secretsvaultcloud.com/v1/token"
+# cred = {'grant_type':'password', 'username':'', 'password':'', 'provider':'thy-one'}
+# response = requests.post(tok_url, data=cred)
+# res = response.json()
+# tok = res['accessToken']
 
-api_url = "https://myvault.secretsvaultcloud.com/v1/secrets/demo/db/postgres/dyna"
-headers =  {"Content-Type":"application/json", "Authorization": tok }
-response = requests.get(api_url, headers=headers)
-#print(response.json())
-res = response.json()
-dbuser = res['data']['credentials']['username']
-dbpass = res['data']['credentials']['password']
-conurl = res['data']['credentials']['connectionURL']
-dbhost = conurl.rpartition('?')[0].rpartition(':')[0]
-dbname = conurl.rpartition('?')[0].rpartition('/')[2]
-print(dbhost, dbname, dbuser, dbpass)
+# api_url = "https://myvault.secretsvaultcloud.com/v1/secrets/demo/db/postgres/dyna"
+# headers =  {"Content-Type":"application/json", "Authorization": tok }
+# response = requests.get(api_url, headers=headers)
+# #print(response.json())
+# res = response.json()
+# dbuser = res['data']['credentials']['username']
+# dbpass = res['data']['credentials']['password']
+# conurl = res['data']['credentials']['connectionURL']
+# dbhost = conurl.rpartition('?')[0].rpartition(':')[0]
+# dbname = conurl.rpartition('?')[0].rpartition('/')[2]
+# print(dbhost, dbname, dbuser, dbpass)
+
+# DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+#     dbuser=res['data']['credentials']['username'],
+#     dbpass=res['data']['credentials']['password'],
+#     dbhost=conurl.rpartition('?')[0].rpartition(':')[0],
+#     dbname=conurl.rpartition('?')[0].rpartition('/')[2]
+# )
+# print(DATABASE_URI)
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=res['data']['credentials']['username'],
-    dbpass=res['data']['credentials']['password'],
-    dbhost=conurl.rpartition('?')[0].rpartition(':')[0],
-    dbname=conurl.rpartition('?')[0].rpartition('/')[2]
+   dbuser=os.environ['DBUSER'],
+   dbpass=os.environ['DBPASS'],
+   dbhost=os.environ['DBHOST'],
+   dbname=os.environ['DBNAME']
 )
-print(DATABASE_URI)
-
-#DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-#    dbuser=os.environ['DBUSER'],
-#    dbpass=os.environ['DBPASS'],
-#    dbhost=os.environ['DBHOST'],
-#    dbname=os.environ['DBNAME']
-#)
 
 TIME_ZONE = 'UTC'
 
